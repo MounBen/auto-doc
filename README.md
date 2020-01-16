8ab20ad24d517170aadf330cfb17e285e3eaa361
MounBen <41362408+MounBen@users.noreply.github.com>
2020-01-16 10:26:22 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# auto-doc
+This repo only serves as a basis to learn how to automate documentation building and publishing on github pages.
+
+It will contain a couple of dummy modules that will almost surely be useless to anyone :)
+
+Current status: can build in the vm
+
+Next steps: - extract from vm and push to /docs
+            - set up gh-pages repo
+            - push to gh-pages instead of /docs
---
+++ dummy_api/class1.py
@@ -0,0 +1,49 @@
+"""
+This is the first class defined in this API!
+"""
+
+
+class Class1:
+
+    def __init__(self, name: str):
+        """
+        Description of the class Class1.
+
+        Ths class does nothing in particular, it only contains a name given at initialization.
+
+        :param name: A string, the name to be given to the Class1 instance.
+        """
+
+        self.name = name
+
+    def my_name_is(self):
+        """
+        Completes the sentence 'My name is ' with the `name` of the class
+
+        This very complicated method relies on a Class1 object to make a sentence.
+        
+        
+        :return: A string which is a sentence.
+        """
+
+        return 'My name is ' + self.name
+
+    def guess_my_name(self, name: str):
+        """
+        Checks if `name` is the current Class1's name.
+
+        If the name is correct, returns 'You guessed my name, you're a genius.'
+        If not, return 'Try again!'
+
+        :param name: A string, the name to test again Class1's name
+        :return: A string indicating whether the guessed name was correct or not
+        """
+
+        answer = ''
+
+        if self.name == name:
+            answer = "You guessed my name, you're a genius"
+        else:
+            answer = 'Try again'
+
+        return answer
